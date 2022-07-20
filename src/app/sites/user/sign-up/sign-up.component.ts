674686c2c49667f4779ddd029c427cee50dd3103
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 21:17:15 +0200

    SEO for sign-up page
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/sign-up/sign-up.component.ts
+++ src/app/sites/user/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+//SEO
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+    //Meta tags
+    @Inject(Meta) private meta: any,
+    //Title
+    @Inject(Title) private title: any,
+  ) { }
+
+
+
+
 
   ngOnInit(): void {
+
+
+
+    //Adding meta tags to the page
+    this.meta.addTags([
+
+      //Description
+      {
+        name: "description", content: "Create your Tranquility community car rental profile for free!"
+      },
+
+      //Keywords
+      {
+        name: "keywords", contents: "Tranquility, sign up, create profile"
+      },
+
+      //End of addMetaTags
+    ]);
+
+
+
+    //Adding a title to the page
+    this.title.setTitle("Tranquility: Sign Up");
+
+
+    //End of ngOnInit
   }
+
+
+
+
 
 }
