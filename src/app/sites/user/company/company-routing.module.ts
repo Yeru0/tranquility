b7ac8679ef2b8d3d11c27be635e74f0a61e9efe8
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 20:22:01 +0200

    Generating car module
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/company/company-routing.module.ts
+++ src/app/sites/user/company/company-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompanyComponent } from './company.component';
 
-const routes: Routes = [{ path: '', component: CompanyComponent }, { path: 'register-company', loadChildren: () => import('./register-company/register-company.module').then(m => m.RegisterCompanyModule) }];
+const routes: Routes = [{ path: '', component: CompanyComponent }, { path: 'register-company', loadChildren: () => import('./register-company/register-company.module').then(m => m.RegisterCompanyModule) }, { path: 'car', loadChildren: () => import('./car/car.module').then(m => m.CarModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
