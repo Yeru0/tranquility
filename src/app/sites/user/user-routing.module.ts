cbb1002299a2ab8ecd536f24968f16cff60035e7
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 20:10:23 +0200

    Generating sign up module
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/user-routing.module.ts
+++ src/app/sites/user/user-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }, { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) }];
+const routes: Routes = [{ path: '', component: UserComponent }, { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) }, { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
