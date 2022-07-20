618579fb0c87c80ade1989252cccbb15dc779268
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 20:54:36 +0200

    Home page SEO
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/home/home.component.ts
+++ src/app/sites/home/home.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+//SEO
+import { Meta, Title } from '@angular/platform-browser';
+
+
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+    //Meta tags
+    @Inject(Meta) private meta: any,
+    //Title
+    @Inject(Title) private title: any
+  ) { }
+
+
+
+
 
   ngOnInit(): void {
+
+
+
+    //Adding meta tags to the page
+    this.meta.addTags([
+
+      //Description
+      {
+        name: "description", content: "Tranquility community car rental website"
+      },
+
+      //Keywords
+      {
+        name: "keywords", content: "Tranquility, community car rental, car rental"
+      }
+
+    ]);
+
+    //Adding a title to the page
+    this.title.setTitle("Tranquility: Home");
+
+
+
+    //End of ngOnInit
   }
+
+
+
+
 
 }
