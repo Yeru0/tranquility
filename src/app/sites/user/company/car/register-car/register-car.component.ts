6d7d4ea4805d842f761fd9e0f083afa3905ddc4a
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 21:47:36 +0200

    SEO for register car
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/company/car/register-car/register-car.component.ts
+++ src/app/sites/user/company/car/register-car/register-car.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+//SEO
+import { Meta, Title } from "@angular/platform-browser";
+
+
+
+
 
 @Component({
   selector: 'app-register-car',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterCarComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+    //Meta tags
+    @Inject(Meta) private meta: any,
+    //Title
+    @Inject(Title) private title: any,
+  ) { }
+
+
+
+
 
   ngOnInit(): void {
+
+
+
+    //Adding meta tags to the page
+    this.meta.addTags([
+
+      //Description
+      {
+        name: "description", content: "Register and link a car to your Tranquility community car rental company"
+      },
+
+      //Keywords
+      {
+        name: "keywords", contents: "Tranquility, car, register, register car"
+      },
+
+      //End of addMetaTags
+    ]);
+
+
+
+    //Adding a title to the page
+    this.title.setTitle("Tranquility: Free Trial");
+
+
+    //End of ngOnInit
   }
+
+
+
+
 
 }
