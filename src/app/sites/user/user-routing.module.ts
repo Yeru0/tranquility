bd33bdd90c308cbe8a35968930a73f63956bff03
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 20:15:28 +0200

    Fixing company module routing bug
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/user-routing.module.ts
+++ src/app/sites/user/user-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }, { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) }, { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: 'trial', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) }];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
