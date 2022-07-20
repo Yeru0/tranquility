10d689f8baa7be802157ae4407bdd471f62e7d49
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 20:11:44 +0200

    Generating trial (free trial) module
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./sites/home/home.module').then(m => m.HomeModule) }, { path: 'user', loadChildren: () => import('./sites/user/user.module').then(m => m.UserModule) }];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./sites/home/home.module').then(m => m.HomeModule) }, { path: 'user', loadChildren: () => import('./sites/user/user.module').then(m => m.UserModule) }, { path: 'trial', loadChildren: () => import('./sites/trial/trial.module').then(m => m.TrialModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
