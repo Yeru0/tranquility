366ca5aa8f600a5eb071af78a81df3cd171e9223
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 21:06:21 +0200

    SEO for trial (free trial) page
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/trial/trial.component.ts
+++ src/app/sites/trial/trial.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+//SEO
+import { Meta, Title } from "@angular/platform-browser";
+
+
+
+
 
 @Component({
   selector: 'app-trial',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrialComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+    //Meta tags
+    @Inject(Meta) private meta: any,
+    //Title
+    @Inject(Title) private title: any,
+  ) { }
+
+
+
+
 
   ngOnInit(): void {
+
+
+
+    //Adding meta tags to the page
+    this.meta.addTags([
+
+      //Description
+      {
+        name: "description", content: "Try out Tranquility community car rental website for free!"
+      },
+
+      //Keywords
+      {
+        name: "keywords", contents: "Tranquility, free trial"
+      },
+
+      //End of addMetaTags
+    ]);
+
+
+
+    //Adding a title to the page
+    this.title.setTitle("Tranquility: Free Trial");
+
+
+    //End of ngOnInit
   }
 
 }
