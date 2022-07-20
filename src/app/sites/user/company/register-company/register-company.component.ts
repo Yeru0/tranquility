db74f9257d31d5afe9bc4611be7bdde3b333380a
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 21:37:45 +0200

    SEO for register company page
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/company/register-company/register-company.component.ts
+++ src/app/sites/user/company/register-company/register-company.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+//SEO
+import { Meta, Title } from "@angular/platform-browser";
+
+
+
+
 
 @Component({
   selector: 'app-register-company',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterCompanyComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+    //Meta tags
+    @Inject(Meta) private meta: any,
+    //Title
+    @Inject(Title) private title: any,
+  ) { }
+
+
+
+
 
   ngOnInit(): void {
+
+
+
+    //Adding meta tags to the page
+    this.meta.addTags([
+
+      //Description
+      {
+        name: "description", content: "Register and link a company to your Tranquility community car rental page account."
+      },
+
+      //Keywords
+      {
+        name: "keywords", contents: "Tranquility, register company"
+      },
+
+      //End of addMetaTags
+    ]);
+
+
+
+    //Adding a title to the page
+    this.title.setTitle("Tranquility: Register Company");
+
+
+    //End of ngOnInit
   }
+
+
+
+
 
 }
