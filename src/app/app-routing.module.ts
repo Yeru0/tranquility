ceea8eab5c0a462282914afbb2bb117bc5cabd21
Horváth Péter <yerulearn065077@gmail.com>
2022-07-20 20:07:38 +0200

    Generating user module
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./sites/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./sites/home/home.module').then(m => m.HomeModule) }, { path: 'user', loadChildren: () => import('./sites/user/user/user.module').then(m => m.UserModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
