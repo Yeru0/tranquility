121504ff99c86ed077fa12e17ec811ee81c5c278
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 10:53:22 +0200

    Company Route decoration
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/company/company-routing.module.ts
+++ src/app/sites/user/company/company-routing.module.ts
@@ -2,7 +2,23 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CompanyComponent } from "./company.component";
 
-const routes: Routes = [{ path: "", component: CompanyComponent }, { path: "register-company", loadChildren: () => import("./register-company/register-company.module").then(m => m.RegisterCompanyModule) }, { path: "car", loadChildren: () => import("./car/car.module").then(m => m.CarModule) }];
+const routes: Routes = [
+  {
+    path: "",
+    component: CompanyComponent
+  },
+
+  {
+    path: "register-company",
+    loadChildren: () => import("./register-company/register-company.module").then(m => m.RegisterCompanyModule)
+  },
+
+  {
+    path: "car",
+    loadChildren: () => import("./car/car.module").then(m => m.CarModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
