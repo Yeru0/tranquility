18d322b4bf72774e03c8602f43239b82a3a0112a
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 16:35:13 +0200

    Solve change password merge confilct
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/auth/change-password/change-password.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ChangePasswordDirective } from './change-password.directive';
-
-describe('ChangePasswordDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ChangePasswordDirective();
-    expect(directive).toBeTruthy();
-  });
-});
