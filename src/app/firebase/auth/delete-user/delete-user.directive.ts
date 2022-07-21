022dc3cb53d77b4439b71439021b7aeed2373ee2
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 19:52:42 +0200

    Firebase auth delete user service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/auth/delete-user/delete-user.directive.ts
+++ src/app/firebase/auth/delete-user/delete-user.directive.ts
@@ -1,10 +1,55 @@
-import { Directive } from '@angular/core';
+import {
+  Directive, Inject,
+  HostListener,
+  EventEmitter, Output
+} from '@angular/core';
+
+import {
+  Auth,
+  deleteUser,
+} from "@angular/fire/auth";
+
+
+
+
 
 @Directive({
   selector: '[appDeleteUser]'
 })
 export class DeleteUserDirective {
 
-  constructor() { }
+
+
+  //Output message
+  @Output() outputMessage = new EventEmitter<string>();
+
+
+
+  constructor(
+    @Inject(Auth) private auth: any
+  ) { }
+
+
+
+  //Click listener
+  @HostListener("click") onClick() {
+
+
+    //User reference
+    const user = this.auth.currentUser;
+
+    //Delete user
+    deleteUser(user)
+      .then((result) => {
+        outputMessage.emit(result);
+      }).catch((err) => {
+        outputMessage.emit(err);
+      });
+
+
+    //End of HostListener
+  }
+
+
 
 }
--- src/app/sites/user/user.module.ts
+++ src/app/sites/user/user.module.ts
@@ -7,16 +7,14 @@ import { UserComponent } from "./user.component";
 import { DndDirective } from '../../firebase/storage/upload/dnd/dnd.directive';
 import { SignOutDirective } from '../../firebase/auth/sign-out/sign-out.directive';
 import { DeleteUserDirective } from '../../firebase/auth/delete-user/delete-user.directive';
-import { ChangePasswordDirective } from '../../firebase/auth/change-password/change-password.directive';
 
 
 @NgModule({
   declarations: [
-    DndDirective,
     UserComponent,
-    SignOutDirective,
+    DndDirective,
     DeleteUserDirective,
-    ChangePasswordDirective,
+    SignOutDirective,
   ],
   imports: [
     CommonModule,
