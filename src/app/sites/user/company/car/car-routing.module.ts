b2f149b6b3b54de637d830b8624e27582e0ead7f
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 11:16:15 +0200

    Car route decoration
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/company/car/car-routing.module.ts
+++ src/app/sites/user/company/car/car-routing.module.ts
@@ -2,7 +2,20 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CarComponent } from "./car.component";
 
-const routes: Routes = [{ path: "", component: CarComponent }, { path: "register-car", loadChildren: () => import("./register-car/register-car.module").then(m => m.RegisterCarModule) }];
+const routes: Routes = [
+
+  //Car route
+  {
+    path: "", component: CarComponent
+  },
+
+  //Register car route
+  {
+    path: "register-car",
+    loadChildren: () => import("./register-car/register-car.module").then(m => m.RegisterCarModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
