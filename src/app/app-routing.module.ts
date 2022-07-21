aea96ab7d328f5aaa3b43f3dfe3e03b5b9dd0156
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 09:47:48 +0200

    Routing for app module
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
+
+//Auth guard
+import { AuthGuard, redirectUnauthorizedTo } from "@angular/fire/auth-guard";
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['user/login']);
+
+
+
 const routes: Routes = [
+
+  //Redirect to home
   { path: "", pathMatch: "full", redirectTo: "home" },
+
+
+
+  //Lazy load home module
   { path: 'home', loadChildren: () => import('./sites/home/home.module').then(m => m.HomeModule) },
+
+  //Lazy load user module
   { path: 'user', loadChildren: () => import('./sites/user/user.module').then(m => m.UserModule) },
-  { path: 'trial', loadChildren: () => import('./sites/trial/trial.module').then(m => m.TrialModule) },
+
+  //Lazy load free trial module
+  {
+    path: 'trial',
+    loadChildren: () => import('./sites/trial/trial.module').then(m => m.TrialModule),
+    canActivate: [AuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }
+  },
+
+  //End of Routes
 ];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
