e708f4baafecfeec88621a9aa06450d9ddfec9d1
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 14:21:00 +0200

    Create firestore delete service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/firestore/functions/delete/delete.service.ts
+++ src/app/firebase/firestore/functions/delete/delete.service.ts
@@ -1,9 +1,53 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
+import {
+  Firestore,
+  doc, deleteDoc
+} from "@angular/fire/firestore";
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeleteService {
 
-  constructor() { }
+
+
+
+
+  constructor(
+    @Inject(Firestore) private db,
+  ) { }
+
+
+
+  delete(collectionFromDelete: string, id: string) {
+
+
+    return new Promise((resolve, reject) => {
+
+      //Doc ref
+      const docRef = doc(this.db, collectionFromDelete, id);
+
+      //Delete the doc
+      deleteDoc(docRef)
+        .then((result) => {
+          resolve(result);
+        }).catch((err) => {
+          reject(err);
+        });
+
+
+      //Promise end
+    });
+
+
+    //Delete function end
+  }
+
+
+
 }
