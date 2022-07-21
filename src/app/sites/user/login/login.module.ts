c6508e296b65b5fa085c2e97c1a0c2ea1746d189
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 15:04:06 +0200

    Updating firebase auth login service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/login/login.module.ts
+++ src/app/sites/user/login/login.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from "@angular/common";
 
 import { LoginRoutingModule } from "./login-routing.module";
 import { LoginComponent } from "./login.component";
+import { GoogleDirective } from '../../../firebase/auth/login/google/google.directive';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    GoogleDirective
   ],
   imports: [
     CommonModule,
