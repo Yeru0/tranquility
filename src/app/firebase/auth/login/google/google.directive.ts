13d2d2070682d3ea93d73d4480668c1ac3b21ac9
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 21:51:52 +0200

    Creating Firebase auth login with google service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/auth/login/google/google.directive.ts
+++ src/app/firebase/auth/login/google/google.directive.ts
@@ -1,10 +1,59 @@
-import { Directive } from '@angular/core';
+import {
+  Directive, Inject,
+  HostListener,
+  Output, EventEmitter
+} from '@angular/core';
+
+
+import {
+  Auth,
+  signInWithPopup, GoogleAuthProvider
+} from "@angular/fire/auth";
+
+
+
+
 
 @Directive({
   selector: '[appGoogle]'
 })
 export class GoogleDirective {
 
-  constructor() { }
+
+
+  //Output message
+  @Output() outputMessage = new EventEmitter<any>();
+
+  //Set provider
+  provider: any;
+
+
+
+  constructor(
+    @Inject(Auth) private auth: any
+  ) {
+    //Set provider
+    this.provider = new GoogleAuthProvider();
+  }
+
+
+
+  //Listen to click envent
+  @HostListener("click") onClick() {
+
+
+    //Logging in with popup windiw
+    signInWithPopup(this.auth, this.provider)
+      .then((result) => {
+        this.outputMessage.emit(result);
+      }).catch((err) => {
+        this.outputMessage.emit(err);
+      });
+
+
+    //HostListener End
+  };
+
+
 
 }
