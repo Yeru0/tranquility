5c662ecb175e37941084d1a9bb9c6e505b008d38
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 10:55:15 +0200

    Company Route decoration (but now with ✨Sparkles✨)
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/user/company/company-routing.module.ts
+++ src/app/sites/user/company/company-routing.module.ts
@@ -2,23 +2,32 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CompanyComponent } from "./company.component";
 
+
+
 const routes: Routes = [
+
+  //Company route
   {
     path: "",
     component: CompanyComponent
   },
 
+  //Register company route
   {
     path: "register-company",
     loadChildren: () => import("./register-company/register-company.module").then(m => m.RegisterCompanyModule)
   },
 
+  //Car route
   {
     path: "car",
     loadChildren: () => import("./car/car.module").then(m => m.CarModule)
   },
 
+  //End of Routes
 ];
+
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
