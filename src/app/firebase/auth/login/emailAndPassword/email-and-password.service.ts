dc4ce4258c8b2c1970427f7ac7461cd37d448e4d
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 21:34:26 +0200

    Creating login with email and password service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/auth/login/emailAndPassword/email-and-password.service.ts
+++ src/app/firebase/auth/login/emailAndPassword/email-and-password.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
+import {
+  Auth,
+  loginWithEmailAndPassword
+} from "@angular/fire/auth";
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailAndPasswordService {
 
-  constructor() { }
+
+
+  constructor(
+    @Inject(Auth) private auth: any
+  ) { }
+
+
+
+  login(email, password) {
+
+
+    return new Promise((resolve, reject) => {
+
+
+      //Login with email and password
+      loginWithEmailAndPassword(this.auth, email, password)
+        .then((result) => {
+          resolve(result);
+        }).catch((err) => {
+          reject(err);
+        });
+
+
+      //Promise end
+    });
+
+    //Login function end
+  }
+
+
 }
