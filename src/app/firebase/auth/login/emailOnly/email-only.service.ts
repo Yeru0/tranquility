275085601878e66c068f72e7e8d7f53ebf468dab
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 21:41:43 +0200

    Creating Firebase auth email only service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/auth/login/emailOnly/email-only.service.ts
+++ src/app/firebase/auth/login/emailOnly/email-only.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
+import {
+  Auth,
+  sendSingInLinkToEmail
+} from "@angular/fire/auth";
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailOnlyService {
 
-  constructor() { }
+  constructor(
+    @Inject(Auth) private auth: any
+  ) { }
+
+  login(email) {
+
+    alert("Logging in with email only is still in a dummy state!");
+
+    return new Promise((result, reject) => {
+
+      const actionCodeSettings = {
+        url: "127.0.0.1/home",
+        handleCodeInApp: true,
+      };
+
+      sendSingInLinkToEmail(this.auth, email, actionCodeSettings)
+        .then((result) => {
+          resolve(result);
+        }).catch((err) => {
+          reject(err);
+        });
+
+    });
+
+
+  }
+
+
 }
