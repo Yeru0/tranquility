a7ffd42c0df90ab82ae65eba9a44e46e750436c0
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 14:10:31 +0200

    Create firestore update service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/firestore/functions/update/update.service.ts
+++ src/app/firebase/firestore/functions/update/update.service.ts
@@ -1,9 +1,62 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
+import {
+  Firestore,
+  doc, updateDoc
+} from "@angular/fire/firestore";
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
 
-  constructor() { }
+
+
+
+
+  constructor(
+    @Inject(Firestore) private db: any,
+  ) { }
+
+
+
+
+
+  update(collectionToUpdate: string, id: string, dataToUpdate: object) {
+
+
+
+    return new Promise((resolve, reject) => {
+
+
+
+      //Doc ref
+      const docRef = doc(this.db, collectionToUpdate, id);
+
+      //Update doc
+      updateDoc(docRef, dataToUpdate)
+        .then((result) => {
+          resolve(result);
+        }).catch((err) => {
+          reject(err);
+        });
+
+
+
+      //End of promise
+    });
+
+
+
+    //End of update function
+  }
+
+
+
+
+
 }
