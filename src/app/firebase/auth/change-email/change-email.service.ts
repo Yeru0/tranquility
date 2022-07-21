650d4ca7552e599724fb9544aea9a3371f4d7093
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 14:56:44 +0200

    Create Fireauth change email service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/auth/change-email/change-email.service.ts
+++ src/app/firebase/auth/change-email/change-email.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
+import {
+  Auth,
+  updateEmail
+} from "@angular/fire/auth";
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChangeEmailService {
 
-  constructor() { }
+
+
+  constructor(
+    @Inject(Auth) private auth: any
+  ) { }
+
+
+
+
+
+  update(email: string) {
+
+
+
+    return new Promise((resolve, reject) => {
+
+
+
+      //Update email
+      updateEmail(this.auth.currentUser, email)
+        .then((result) => {
+          resolve(result);
+        }).catch((err) => {
+          reject(err);
+        });
+
+
+
+      //End of promise
+    });
+
+
+
+    //End of update function
+  }
+
+
+
 }
