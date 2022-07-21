4d6e8e0e887a4cf7fcf75840e8c81e9d52bf6710
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 12:12:57 +0200

    Generating firebase services and directives
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

---
+++ src/app/firebase/auth/delete-user/delete-user.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appDeleteUser]'
+})
+export class DeleteUserDirective {
+
+  constructor() { }
+
+}
--- src/app/sites/user/user.module.ts
+++ src/app/sites/user/user.module.ts
@@ -4,10 +4,17 @@ import { CommonModule } from "@angular/common";
 import { UserRoutingModule } from "./user-routing.module";
 import { UserComponent } from "./user.component";
 
+import { DndDirective } from '../../firebase/storage/upload/dnd/dnd.directive';
+import { SignOutDirective } from '../../firebase/auth/sign-out/sign-out.directive';
+import { DeleteUserDirective } from '../../firebase/auth/delete-user/delete-user.directive';
+
 
 @NgModule({
   declarations: [
-    UserComponent
+    DndDirective,
+    UserComponent,
+    SignOutDirective,
+    DeleteUserDirective,
   ],
   imports: [
     CommonModule,
