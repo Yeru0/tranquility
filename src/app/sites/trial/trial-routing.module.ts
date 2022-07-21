25da3a6beea42d5318c9564e4cfb0df300f571df
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 11:19:11 +0200

    Trial route decoration
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/sites/trial/trial-routing.module.ts
+++ src/app/sites/trial/trial-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { TrialComponent } from "./trial.component";
 
-const routes: Routes = [{ path: "", component: TrialComponent }];
+const routes: Routes = [
+
+  //Trial route
+  {
+    path: "",
+    component: TrialComponent
+  }
+
+  //End of Routes
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
