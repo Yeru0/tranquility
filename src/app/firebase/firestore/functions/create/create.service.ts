0fcc3481b5a2ddecf7ba992b81c19613473b8473
Horváth Péter <yerulearn065077@gmail.com>
2022-07-21 13:21:09 +0200

    Creating firestore create service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/firestore/functions/create/create.service.ts
+++ src/app/firebase/firestore/functions/create/create.service.ts
@@ -1,9 +1,94 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
+//Firebase
+import {
+  Firestore,
+  collection, addDoc,
+  doc, updateDoc
+} from "@angular/fire/firestore";
+
+//Datr.js
+import * as Date from "datr.js";
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateService {
 
-  constructor() { }
+
+
+
+
+  constructor(
+    @Inject(Firestore) private db: any,
+    @Inject(Date) private date: any,
+  ) { }
+
+
+
+
+
+  add(collectionToAdd: string, dataToAdd: object) {
+
+    return new Promise((resolve, reject) => {
+
+      const colRef = collection(this.db, collectionToAdd);
+
+      addDoc(colRef, dataToAdd)
+        .then((result) => {
+
+
+          //Doc reference
+          const docRef = doc(this.db, collectionToAdd, result.id);
+
+          //Current time
+          const time = this.date.get("YYMDdhhhhmmmssssssZ");
+
+
+
+          //Updating the crated doc with it's ID an the time that it was created
+          updateDoc(docRef, {
+
+            id: result.id,
+            createdAt: time
+
+          })
+            .then((result) => {
+
+              resolve(result);
+
+            }).catch((err) => {
+
+              reject(err);
+
+              //End of updateDo then catch block
+            });
+
+
+
+        }).catch((err) => {
+
+          reject(err);
+
+
+
+          //End of addDoc then catch block
+        });
+
+
+
+      //End of Promise
+    });
+
+
+
+    //End of Add function
+  }
+
+
+
 }
