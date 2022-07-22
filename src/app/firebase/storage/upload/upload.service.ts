5616bdf827afdf04ebde38e5ddc50ff5a8cd4902
Horváth Péter <yerulearn065077@gmail.com>
2022-07-22 11:54:22 +0200

    Creating Firebase storage upload service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/storage/upload/upload.service.ts
+++ src/app/firebase/storage/upload/upload.service.ts
@@ -5,21 +5,39 @@ import {
   ref, uploadBytesResumable
 } from "@angular/fire/storage";
 
+
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class UploadService {
 
+
+
   constructor(
     @Inject(Storage) private storage: any,
   ) { }
 
-  upload(where, name, file) {
 
-    const storageRef = `${where}/${name}`;
 
+  //Upload a fiel
+  upload(where: string, name: string, file: File) {
+
+
+
+    //Storage reference
+    const storageRef = ref(this.storage, `${where}/${name}`);
+
+    //Upload file
     return uploadBytesResumable(storageRef, file);
 
+
+
+    //Upload function end
   }
+
+
 
 }
