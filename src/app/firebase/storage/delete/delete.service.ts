3b9dd0051a49378ff131b7dbbd06520f33ea8d7f
Horváth Péter <yerulearn065077@gmail.com>
2022-07-22 13:02:26 +0200

    Creating Firestore storage delete service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/storage/delete/delete.service.ts
+++ src/app/firebase/storage/delete/delete.service.ts
@@ -1,9 +1,62 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
+import {
+  Storage,
+  ref, deleteObject
+} from "@angular/fire/storage";
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeleteService {
 
-  constructor() { }
+
+
+
+
+  constructor(
+    @Inject(Storage) private storage: any
+  ) { }
+
+
+
+
+
+  delete(path: string, name: string) {
+
+
+
+    return new Promise((resolve, reject) => {
+
+
+
+      //File reference
+      const fileRef = ref(this.storage, `${path}/${name}`);
+
+      //Delete the file
+      deleteObject(fileRef)
+        .then((result) => {
+          resolve(result);
+        }).catch((err) => {
+          reject(err);
+        });
+
+
+
+      //Promise end
+    });
+
+
+
+    //Delete function end
+  }
+
+
+
+
+
 }
