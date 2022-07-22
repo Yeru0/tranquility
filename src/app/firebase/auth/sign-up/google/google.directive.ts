afdf4a2b3376b4af680b915cb40c244219fcf570
Horváth Péter <yerulearn065077@gmail.com>
2022-07-22 10:05:19 +0200

    Create Firebase auth sign up with google directive
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/auth/sign-up/google/google.directive.ts
+++ src/app/firebase/auth/sign-up/google/google.directive.ts
@@ -1,10 +1,55 @@
-import { Directive } from '@angular/core';
+import {
+  Directive, Inject,
+  HostListener,
+  Output, EventEmitter
+} from '@angular/core';
+
+import {
+  Auth,
+  signInWithPopup, GoogleAuthProvider
+} from "@angular/fire/auth";
+
+
+
+
 
 @Directive({
   selector: '[appGoogle]'
 })
 export class GoogleDirective {
 
-  constructor() { }
+
+  //Output message
+  @Output() outputMessage = new EventEmitter<any>();
+
+
+  provider: any;
+
+
+
+  constructor(
+    @Inject(Auth) private auth: any
+  ) {
+    this.provider = new GoogleAuthProvider();
+  }
+
+
+  //Listening ti the click event
+  @HostListener("click") onClick() {
+
+
+    //Signing up with popup
+    signInWithPopup(this.auth, this.provider)
+      .then((result) => {
+        this.outputMessage.emit(result);
+      }).catch((err) => {
+        this.outputMessage.emit(err);
+      });
+
+
+    //HostListener end
+  }
+
+
 
 }
