177f021a963a90f3c66f7cfa34e3d507aa0e7a16
Horváth Péter <yerulearn065077@gmail.com>
2022-07-22 09:45:25 +0200

    Firebase auth password reset email service
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/app/firebase/auth/passwordResetEmail/password-reset-email.service.ts
+++ src/app/firebase/auth/passwordResetEmail/password-reset-email.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
+import {
+  Auth,
+  sendPasswordResetEmail
+} from "@angular/fire/auth";
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PasswordResetEmailService {
 
-  constructor() { }
+
+
+  constructor(
+    @Inject(Auth) private auth: any,
+  ) { }
+
+
+  //Send a password reset email
+  sendEmail(email) {
+
+
+
+    return new Promise((resolve, reject) => {
+
+
+      //Send the email
+      sendPasswordResetEmail(this.auth, email)
+        .then((result) => {
+          resolve(result);
+        }).catch((err) => {
+          reject(err);
+        });
+
+
+
+      //Promise end
+    });
+
+
+
+    //sendEmail function end
+  }
+
+
+
 }
